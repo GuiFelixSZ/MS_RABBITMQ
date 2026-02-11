ba4cad1923e4b30b5c2e37f9e93c39b8b2138c02
Gui <guifelixs@gmail.com>
2026-02-10 23:16:39 -0300

    Removendo print e adicionando conversão dto

--- email/src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,17 +1,27 @@
 package com.ms.email.consumers;
 
 import com.ms.email.dtos.EmailRecordDTo;
-import jakarta.validation.constraints.Email;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
+    final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDTo EmailRecordDTo) {
-        System.out.println(EmailRecordDTo.emailTo());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(EmailRecordDTo, emailModel);
+        emailService.sendEmail(emailModel);
     }
 }
 
